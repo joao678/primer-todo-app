3fe5a15c3ceeeb13a79c6901655f5734f633ee49
fcanudo <joao678@gmail.com>
2024-02-18 14:37:46 -0300

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { DashIcon, PlusIcon } from '@primer/octicons-react';
-import { Box, Button, Checkbox, IconButton, PageLayout, Text, TextInput } from '@primer/react';
-import { DataTable, PageHeader } from '@primer/react/drafts';
-import { useState } from 'react';
+import { Box, Button, Checkbox, IconButton, Text, TextInput } from '@primer/react';
+import { DataTable } from '@primer/react/drafts';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
     const [todoList, setTodoList] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
+    useEffect(() => {
+        const tableBody = document.querySelector('.TableBody');
+
+        tableBody.scrollHeight > tableBody.clientHeight ?
+            tableBody.style.scrollbarGutter = 'stable' :
+            tableBody.style.scrollbarGutter = '';
+    }, [todoList]);
+
     return (
-        <Box m={4} display={'grid'} gridAutoFlow={'row'} gridGap={2} sx={{ overflowWrap: 'anywhere' }}>
+        <Box
+            p={4}
+            display={'grid'}
+            gridAutoFlow={'row'}
+            gridGap={2}
+            gridTemplateRows={'0fr 1fr'}
+            sx={{ height: '100vh' }}>
             <Box display={'grid'} gridAutoFlow={'column'} gridAutoColumns={'1fr 0fr'} gridGap={2}>
                 <TextInput value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
                 <Button variant='primary' trailingVisual={() => <PlusIcon />} onClick={() => {
@@ -17,42 +31,40 @@ function App() {
                     setInputValue('');
                 }}>Add</Button>
             </Box>
-            <Box display={todoList.length ? 'block' : 'none'}>
-                <DataTable
-                    data={todoList}
-                    columns={[{
-                        header: 'Done',
-                        field: 'done',
-                        maxWidth: 70,
-                        align: 'start',
-                        renderCell: (row) => <Checkbox checked={row.done}
-                            onChange={() => {
-                                const id = todoList.findIndex(x => x === row);
-                                todoList[id].done = !todoList[id].done;
-                                setTodoList([...todoList])
-                            }}
-                        />
-                    }, {
-                        header: 'Description',
-                        field: 'text',
-                        width: 'grow',
-                        maxWidth: '100%',
-                        renderCell: (row) => <Text sx={{
-                            // color: row.done ? 'success.fg' : '',
-                            fontWeight: 'bold',
-                            textDecoration: row.done ? 'line-through' : ''
-                        }}>{row.text}</Text>
-                    }, {
-                        header: '',
-                        field: '',
-                        align: 'end',
-                        renderCell: (row) => <IconButton variant='danger' trailingVisual={() => <DashIcon />} onClick={() => {
-                            todoList.splice(todoList.findIndex(x => x === row), 1);
+            <DataTable
+                data={todoList}
+                columns={[{
+                    header: 'Done',
+                    field: 'done',
+                    align: 'start',
+                    width: 'auto',
+                    renderCell: (row) => <Checkbox checked={row.done}
+                        onChange={() => {
+                            const id = todoList.findIndex(x => x === row);
+                            todoList[id].done = !todoList[id].done;
                             setTodoList([...todoList]);
-                        }}></IconButton>
-                    }]}></DataTable >
-            </Box>
-        </Box >
+                        }}
+                    />
+                }, {
+                    header: 'Description',
+                    field: 'text',
+                    width: 'growCollapse',
+                    renderCell: (row) => <Text sx={{
+                        color: row.done ? 'accent.fg' : '',
+                        fontWeight: 'bold',
+                        textDecoration: row.done ? 'line-through' : ''
+                    }}>{row.text}</Text>
+                }, {
+                    header: '',
+                    field: '',
+                    align: 'end',
+                    width: 'auto',
+                    renderCell: (row) => <IconButton variant='danger' trailingVisual={() => <DashIcon />} onClick={() => {
+                        todoList.splice(todoList.findIndex(x => x === row), 1);
+                        setTodoList(todoList.map((x, i) => ({ done: x.done, text: x.text, id: i })));
+                    }} />
+                }]} />
+        </Box>
     )
 }
 
